e7550cc217b6c82534026e39ff6839e56128f454
danialfiaz <95887627+danialfiaz@users.noreply.github.com>
2024-04-26 19:12:27 +0500

    Update ingress-controller.sh

--- ingress-controller.sh
+++ ingress-controller.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-# INGRESS_NAME=$3
-# ACR_LOGIN_SERVER=$2
- NAMESPACE=$1
-#INGRESS_VALUES_FILE=$4
-#AKS_HELLO=$5
-#INGRESS_DEMO=$6
-#INGRESS_MANIFEST=$7
+INGRESS_NAME=$1
+ACR_LOGIN_SERVER=$2
+NAMESPACE=$3
+# INGRESS_VALUES_FILE=$4
+# AKS_HELLO=$5
+# INGRESS_DEMO=$6
+# INGRESS_MANIFEST=$7
 
 
 
 #acr_registry=<yourregistryname>
-# CONTROLLER_REGISTRY=k8s.gcr.io
-# CONTROLLER_IMAGE=ingress-nginx
-# CONTROLLER_TAG=v0.48.1
-# PATCH_REGISTRY=docker.io
-# PATCH_IMAGE=jettech/kube-webhook-certgen
-# PATCH_TAG=v1.5.1
-# DEFAULTBACKEND_REGISTRY=k8s.gcr.io
-# DEFAULTBACKEND_IMAGE=defaultbackend-amd64
-# DEFAULTBACKEND_TAG=1.5
+CONTROLLER_REGISTRY=k8s.gcr.io
+CONTROLLER_IMAGE=ingress-nginx
+CONTROLLER_TAG=v0.48.1
+PATCH_REGISTRY=docker.io
+PATCH_IMAGE=jettech/kube-webhook-certgen
+PATCH_TAG=v1.5.1
+DEFAULTBACKEND_REGISTRY=k8s.gcr.io
+DEFAULTBACKEND_IMAGE=defaultbackend-amd64
+DEFAULTBACKEND_TAG=1.5
 
 
 # create namespace if doesn't exists
@@ -40,7 +40,7 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 # Set variable for ACR location to use for pulling images
-ACR_LOGIN_SERVER=$(ACR_LOGIN_SERVER)
+ACR_LOGIN_SERVER=$ACR_LOGIN_SERVER
 
 # Use Helm to deploy an NGINX ingress controller
 helm install ingress-nginx ingress-nginx/ingress-nginx \
@@ -49,22 +49,22 @@ helm install ingress-nginx ingress-nginx/ingress-nginx \
     #--create-namespace \
     --set controller.replicaCount=2 \
     --set controller.nodeSelector."kubernetes\.io/os"=linux \
-    --set controller.image.registry=$(ACR_LOGIN_SERVER) \
-    --set controller.image.image=$(CONTROLLER_IMAGE) \
-    --set controller.image.tag=$(CONTROLLER_TAG) \
+    --set controller.image.registry=$ACR_LOGIN_SERVER \
+    --set controller.image.image=$CONTROLLER_IMAGE \
+    --set controller.image.tag=$CONTROLLER_TAG \
     --set controller.image.digest="" \
     --set controller.admissionWebhooks.patch.nodeSelector."kubernetes\.io/os"=linux \
     --set controller.service.loadBalancerIP=10.224.0.42 \
     --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal"=true \
     --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz \
-    --set controller.admissionWebhooks.patch.image.registry=$(ACR_LOGIN_SERVER) \
-    --set controller.admissionWebhooks.patch.image.image=$(PATCH_IMAGE) \
-    --set controller.admissionWebhooks.patch.image.tag=$(PATCH_TAG) \
+    --set controller.admissionWebhooks.patch.image.registry=$ACR_LOGIN_SERVER \
+    --set controller.admissionWebhooks.patch.image.image=$PATCH_IMAGE \
+    --set controller.admissionWebhooks.patch.image.tag=$PATCH_TAG \
     --set controller.admissionWebhooks.patch.image.digest="" \
     --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux \
-    --set defaultBackend.image.registry=$(ACR_LOGIN_SERVER) \
-    --set defaultBackend.image.image=$(DEFAULTBACKEND_IMAGE) \
-    --set defaultBackend.image.tag=$(DEFAULTBACKEND_TAG) \
+    --set defaultBackend.image.registry=$ACR_LOGIN_SERVER \
+    --set defaultBackend.image.image=$DEFAULTBACKEND_IMAGE \
+    --set defaultBackend.image.tag=$DEFAULTBACKEND_TAG \
     --set defaultBackend.image.digest=""
 
 
