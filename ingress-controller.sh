5c67597dd7adba920424369fa19b7df81fd66469
danialfiaz <95887627+danialfiaz@users.noreply.github.com>
2024-04-26 21:51:30 +0500

    Update ingress-controller.sh

--- ingress-controller.sh
+++ ingress-controller.sh
@@ -10,13 +10,21 @@ NAMESPACE=$3
 
 
 #acr_registry=<yourregistryname>
-CONTROLLER_REGISTRY=k8s.gcr.io
-CONTROLLER_IMAGE=ingress-nginx
-CONTROLLER_TAG=v0.48.1
-PATCH_REGISTRY=docker.io
-PATCH_IMAGE=jettech/kube-webhook-certgen
-PATCH_TAG=v1.5.1
-DEFAULTBACKEND_REGISTRY=k8s.gcr.io
+# CONTROLLER_REGISTRY=k8s.gcr.io
+# CONTROLLER_IMAGE=ingress-nginx
+# CONTROLLER_TAG=v0.48.1
+# PATCH_REGISTRY=docker.io
+# PATCH_IMAGE=jettech/kube-webhook-certgen
+# PATCH_TAG=v1.5.1
+# DEFAULTBACKEND_REGISTRY=k8s.gcr.io
+# DEFAULTBACKEND_IMAGE=defaultbackend-amd64
+# DEFAULTBACKEND_TAG=1.5
+
+SOURCE_REGISTRY=registry.k8s.io
+CONTROLLER_IMAGE=ingress-nginx/controller
+CONTROLLER_TAG=v1.8.1
+PATCH_IMAGE=ingress-nginx/kube-webhook-certgen
+PATCH_TAG=v20230407
 DEFAULTBACKEND_IMAGE=defaultbackend-amd64
 DEFAULTBACKEND_TAG=1.5
 
@@ -29,11 +37,11 @@ export HELM_EXPERIMENTAL_OCI=1
 TOKEN=$(az acr login --name $ACR_LOGIN_SERVER --expose-token --output tsv --query accessToken)
 echo $TOKEN | helm registry login $ACR_LOGIN_SERVER --username 00000000-0000-0000-0000-000000000000 --password-stdin
 
-# # Pull Helm Chart from Azure Container Registry & extract files
-# helm pull oci://$ACR_URL/<pathofregistry> --version 3.36.0 --untar
+# Pull Helm Chart from Azure Container Registry & extract files
+helm pull oci://$ACR_URL/<pathofregistry> --version 4.7.1 --untar
 
 #validate chart is desired version
-helm show chart ingress-nginx
+# helm show chart ingress-nginx
 
 # Add the ingress-nginx repository
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
