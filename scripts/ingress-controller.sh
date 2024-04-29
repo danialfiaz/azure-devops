528b7af57ceb0454b6ff8abda2c8ec056dcbe249
danialfiaz <95887627+danialfiaz@users.noreply.github.com>
2024-04-29 20:07:53 +0500

    Update ingress-controller.sh

--- scripts/ingress-controller.sh
+++ scripts/ingress-controller.sh
@@ -48,11 +48,11 @@ helm repo update
 ACR_LOGIN_SERVER=$ACR_LOGIN_SERVER
 
 # Use Helm to deploy an NGINX ingress controller
-helm install ingress-nginx ingress-nginx-1/ingress-nginx-1 \
+helm install ingress-nginx ingress-nginx/ingress-nginx \
     --version 4.7.1 \
     --namespace $NAMESPACE \
     --set controller.replicaCount=2 \
     --set controller.nodeSelector."kubernetes\.io/os"=linux \
     --set controller.admissionWebhooks.patch.nodeSelector."kubernetes\.io/os"=linux \
-    --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal"=true
+    --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal"=true \
     --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz
